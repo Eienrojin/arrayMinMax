fdc3714e596823e867ea5bcbb854f05c80afbf86
Nikita Polunin (二木立・半人) <Stkchanelx@gmail.com>
2021-11-18 21:42:40 +0300

    Добавьте файлы проекта.

---
+++ arrayMinMax/arrayMinMax.cpp
@@ -0,0 +1,62 @@
+﻿//11.122.В массиве хранится информация о стоимости каждой из 60 книг. Определить количество самых дешевых книг
+
+#include <iostream>
+using namespace std;
+
+//variables
+int* arrayMinMax;
+const int LENGTH = 60;
+int countCheapBook = 0;
+
+//functions
+void fillRandArray();
+void printArray();
+int getMinNum(int *arrayMinMax);
+
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+    fillRandArray();
+	printArray();
+
+	cout << endl << "Минимальное число: " << getMinNum(arrayMinMax);
+	cout << endl << "Количество самых дешевых книг: " << countCheapBook;
+
+}
+
+void fillRandArray()
+{
+	arrayMinMax = new int[LENGTH];
+
+	for (int i = 0; i < LENGTH; i++)
+	{
+		arrayMinMax[i] = 100 + rand() % 200;
+	}
+}
+
+void printArray()
+{
+	cout << "Элементы массива: ";
+	for (int i = 0; i < LENGTH; i++)
+	{
+		cout << "|" << arrayMinMax[i];
+	}
+}
+
+int getMinNum(int* arrayMinMax)
+{
+	int minNum = arrayMinMax[0];
+	for (int i = 0; i < LENGTH; i++)
+	{
+		if (arrayMinMax[i] < minNum || arrayMinMax[i] == minNum)
+		{
+			minNum = arrayMinMax[i];
+
+			countCheapBook++;
+		}
+	}
+
+	return minNum;
+}
